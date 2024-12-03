22e540b916b4b05b80c1099257213214b1959bc1
ZorpEngur <zoprengur@gmail.com>
2024-12-03 21:00:59 +0100

    Day3 -omgwtf

--- src/Day3/Part2.java
+++ src/Day3/Part2.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 public class Part2 {
     public static void main(String[] args) throws IOException {
         String string = Files.lines(Path.of("src/Day3/input.txt")).reduce(String::concat).get();
-        Matcher input = Pattern.compile("mul\\((?<num0>\\d{1,3}),(?<num2>\\d{1,3})\\)|(?<do>do\\(\\))|(?<dont>don't\\(\\))").matcher(string);
+        Matcher input = Pattern.compile("mul\\((?<num1>\\d{1,3}),(?<num2>\\d{1,3})\\)|(?<do>do\\(\\))|(?<dont>don't\\(\\))").matcher(string);
         boolean isDo = true;
         int result = 0;
         for (MatchResult match : input.results().toList()) {
@@ -19,7 +19,7 @@ public class Part2 {
             } else if (match.group("do") != null) {
                 isDo = true;
             } else if (isDo) {
-                result += Integer.parseInt(match.group("num0")) * Integer.parseInt(match.group("num2"));
+                result += Integer.parseInt(match.group("num1")) * Integer.parseInt(match.group("num2"));
             }
         }
         System.out.println(result);
