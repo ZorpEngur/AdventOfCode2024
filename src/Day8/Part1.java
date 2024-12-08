ec42d4521e147e63ab68a3e4a83a1ef6c15e6ac3
ZorpEngur <zoprengur@gmail.com>
2024-12-08 12:03:45 +0100

    Day8

---
+++ src/Day8/Part1.java
@@ -0,0 +1,57 @@
+package Day8;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Part1 {
+
+    private static List<String> test = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.lines(Path.of("src/Day8/input.txt")).toList();
+        test = new ArrayList<>(lines);
+        Map<Character, List<Map.Entry<Integer, Integer>>> antennas = new HashMap<>();
+        for (int i = 0; i < lines.size(); i++) {
+            for (int x = 0; x < lines.get(i).length(); x++) {
+                if (lines.get(i).charAt(x) != '.') {
+                    antennas.computeIfAbsent(lines.get(i).charAt(x), c -> new ArrayList<>()).add(Map.entry(i, x));
+                }
+            }
+        }
+        checkAntinodes(findAntinodes(antennas), lines.get(0).length(), lines.size());
+    }
+
+    private static List<Map.Entry<Integer, Integer>> findAntinodes(Map<Character, List<Map.Entry<Integer, Integer>>> antennas) {
+        List<Map.Entry<Integer, Integer>> antinodes = new ArrayList<>();
+        for (List<Map.Entry<Integer, Integer>> pos : antennas.values()) {
+            for (int i = 0; i < pos.size(); i++) {
+                for (int y = i + 1; y < pos.size(); y++) {
+                    int yDiff = Math.abs(pos.get(i).getKey() - pos.get(y).getKey());
+                    int xDiff = Math.abs(pos.get(i).getValue() - pos.get(y).getValue());
+                    int yMod = pos.get(i).getKey() > pos.get(y).getKey() ? 1 : -1;
+                    int xMod = pos.get(i).getValue() > pos.get(y).getValue() ? 1 : -1;
+                    antinodes.add(Map.entry(pos.get(i).getKey() + yDiff * yMod, pos.get(i).getValue() + xDiff * xMod));
+                    antinodes.add(Map.entry(pos.get(y).getKey() - yDiff * yMod, pos.get(y).getValue() - xDiff * xMod));
+                }
+            }
+        }
+        return antinodes;
+    }
+
+    private static void checkAntinodes(List<Map.Entry<Integer, Integer>> antinodes, int lineLength, int rows) {
+        for (Map.Entry<Integer, Integer> antinode : antinodes) {
+            if (antinode.getKey() < 0 || antinode.getKey() >= rows || antinode.getValue() < 0 || antinode.getValue() >= lineLength) {
+                continue;
+            }
+            char[] line = test.get(antinode.getKey()).toCharArray();
+            line[antinode.getValue()] = '#';
+            test.set(antinode.getKey(), new String(line));
+        }
+        test.forEach(System.out::println);
+        System.out.println(test.stream().map(e -> e.chars().filter(x -> x == '#').count()).reduce(Long::sum));
+    }
+}
