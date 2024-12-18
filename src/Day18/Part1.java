6f8ea88ccb4c10f3f0e8c5a6fb7e3343f9283113
ZorpEngur <zoprengur@gmail.com>
2024-12-18 09:08:17 +0100

    Day18

---
+++ src/Day18/Part1.java
@@ -0,0 +1,40 @@
+package Day18;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Part1 {
+
+    private static Map<Map.Entry<Integer, Integer>, Integer> shortestPath = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        List<String> input = Files.lines(Path.of("src/Day18/input.txt")).toList();
+        int width = 70;
+        int height = 70;
+        Set<Map.Entry<Integer, Integer>> corruptedMemory = input.stream().map(e -> {
+            String[] pos = e.split(",");
+            return Map.entry(Integer.parseInt(pos[0]), Integer.parseInt(pos[1]));
+        }).limit(1024).collect(Collectors.toSet());
+        Queue<Map.Entry<Integer, Integer>> queue = new ArrayDeque<>();
+        queue.add(Map.entry(0, 0));
+        shortestPath.put(Map.entry(0, 0), 0);
+        while (!queue.isEmpty()) {
+            Map.Entry<Integer, Integer> pos = queue.poll();
+            int distance = shortestPath.get(pos);
+            for (Map.Entry<Integer, Integer> dir : List.of(Map.entry(0, 1), Map.entry(0, -1), Map.entry(1, 0), Map.entry(-1, 0))) {
+                Map.Entry<Integer, Integer> newPos =  Map.entry(pos.getKey() + dir.getKey(), pos.getValue() + dir.getValue());
+                if (corruptedMemory.contains(newPos) || newPos.getKey() < 0 || newPos.getKey() > width || newPos.getValue() < 0 || newPos.getValue() > height) {
+                    continue;
+                }
+                if (!shortestPath.containsKey(newPos) || distance + 1 < shortestPath.get(newPos)) {
+                    shortestPath.put(newPos, distance + 1);
+                    queue.add(newPos);
+                }
+            }
+        }
+        System.out.println(shortestPath.get(Map.entry(width, height)));
+    }
+}
